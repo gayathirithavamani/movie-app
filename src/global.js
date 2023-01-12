9e1a162658600af5796bca8c7e411fd5ca93531b
gayathirithavamani <gayat2000@gmail.com>
2023-01-12 10:28:05 +0530

    under commit

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 //export const API = "https://63899fdf4eccb986e895a997.mockapi.io";
-//export const API = "http://localhost:4000";
-export const API = "https://node-project-my-hmfd.vercel.app";
+export const API = "http://localhost:4000";
+//export const API = "https://node-project-my-hmfd.vercel.app";
